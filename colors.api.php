2169b043a5ba9169019f2a066625217080d1086a
Tim Plunkett <git@plnktt.com>
2011-12-12 10:54:45 -0500

    Copy over code from FullCalendar Colors.

--- colors.api.php
+++ colors.api.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Hooks provided by the FullCalendar module.
+ * Hooks provided by the Colors module.
  */
 
 /**
@@ -16,7 +16,7 @@
  * @return
  *   Mapping between the colorable features and the CSS input.
  */
-function colors_colors_get_color_mapping() {
+function hook_colors_get_color_mapping() {
   return array(
     'background' => 'background-color',
     'text' =>  'color',
@@ -37,6 +37,33 @@ function hook_colors_build_selector($class) {
   $selector = '.' . $class . ',';
   $selector .= '.' . $class . 'a';
   return $selector;
+}
+
+/**
+ * Declare if the CSS file should be automatically rebuilt.
+ *
+ * @return bool
+ *   TRUE if the file should be rebuilt automatically, FALSE if it should be
+ *   rebuilt at runtime.
+ */
+function hook_colors_rebuild() {
+  return TRUE;
+}
+
+/**
+ * Provide a way for modules to add the classes used to their markup.
+ *
+ * @param object $entity
+ *   The entity object.
+ */
+function hook_colors_classes($entity) {
+  $class_names = array();
+  if (variable_get('colors_node_type_enabled', FALSE)) {
+    if ($entity->entity_type == 'node') {
+      $class_names[] = 'colors-node-type-' . $entity->type;
+    }
+  }
+  return $class_names;
 }
 
 /**
